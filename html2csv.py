29f269812babca4434ed4ff290db996ec6c6e158
David Ginzberg <arguing.with.my.keyboard@gmail.com>
2018-11-15 14:16:35 -0500

    Add nesting table removal and TODOs

--- html2csv.py
+++ html2csv.py
@@ -4,6 +4,8 @@
 #rbarnes@umn.edu
 #
 
+#TODO: Extract TODO comments into github issues
+
 from bs4 import BeautifulSoup
 import sys
 import csv
@@ -14,6 +16,8 @@ parser.add_argument('--delimiter', '-d', action='store', default=',',help="Chara
 parser.add_argument('--quotechar', '-q', action='store', default='"',help="Character within which to nest CSV text")
 parser.add_argument('filename',nargs="?",help="HTML file from which to extract tables")
 args = parser.parse_args()
+
+#TODO: Add a -v/--verbose option for the excessive print statements below. Consider both `-v` `-V` (AKA: VERY VERBOSE, which is currently the default)
 
 if sys.stdin.isatty() and not args.filename:
   parser.print_help()
@@ -39,8 +43,21 @@ for table in soup.findAll("table"):
   print("Processing Table #%d" % (tablecount))
   with open(sys.argv[1]+str(tablecount)+'.csv', 'w', newline='') as csvfile:
     fout = csv.writer(csvfile, delimiter=args.delimiter, quotechar=args.quotechar, quoting=csv.QUOTE_MINIMAL)
+    rowcount = 1
+    #Removes nested tables. for handling the sins of 1990's web pages.
+    #TODO: Add an argument to enable/disable table de-nesting
+    [t.extract() for t in table.findAll("table")]
+    #This would grab all TRs regardless of depth without the above line removing nested tables
     for row in table.findAll('tr'):
+      print(f"Processing row number {rowcount}")
+      rowcount += 1
       cols = row.findAll(['td','th'])
+      print(f"Found {len(cols)} columns.")
+      #TODO: Detect and discard empty tables (those that contain 1 row, 1 column, consisting of an empty string...I think?)
       if cols:
         cols = [str(x.text).strip() for x in cols]
         fout.writerow(cols)
+#TODO: In HTML documents that use tables for layout (yuck!) add option to detect table titles in preceding sibling tables of size 1x1
+#TODO: Add option to compress multi-line categorical headers (eg: headers with colspan>1) into concatenated-naming-style single row headers
+#  EG: "Population" header spanning above sub-headers "Number" and "Percentage" produces two single-column headers of "Population - Number"
+#  and "Population - Percentage". Delemiter can be configurable with a sensible default set.
