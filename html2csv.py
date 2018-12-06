75e5df44a53a8520bd2f129f48a9f4386b578c14
David Ginzberg <arguing.with.my.keyboard@gmail.com>
2018-12-05 19:31:32 -0500

    remove #TODO tags

--- html2csv.py
+++ html2csv.py
@@ -4,7 +4,6 @@
 #rbarnes@umn.edu
 #
 
-#TODO: Extract TODO comments into github issues
 
 from bs4 import BeautifulSoup
 import os
@@ -19,7 +18,6 @@ parser.add_argument('--ignoreempty', '-e', action='store_true', help="Ignore emp
 parser.add_argument('filename',nargs="?",help="HTML file from which to extract tables")
 args = parser.parse_args()
 
-#TODO: Add a -v/--verbose option for the excessive print statements below. Consider both `-v` `-V` (AKA: VERY VERBOSE, which is currently the default)
 
 if sys.stdin.isatty() and not args.filename:
   parser.print_help()
@@ -38,6 +36,8 @@ soup = BeautifulSoup(fin,"html.parser")
 print("Preemptively removing unnecessary tags")
 [s.extract() for s in soup('script')]
 
+
+
 print("CSVing file")
 tablecount = -1
 for table in soup.findAll("table"):
@@ -49,7 +49,6 @@ for table in soup.findAll("table"):
     fout = csv.writer(csvfile, delimiter=args.delimiter, quotechar=args.quotechar, quoting=csv.QUOTE_MINIMAL)
     rowcount = 1
     #Removes nested tables. for handling the sins of 1990's web pages.
-    #TODO: Add an argument to enable/disable table de-nesting
     [t.extract() for t in table.findAll("table")]
     #This would grab all TRs regardless of depth without the above line removing nested tables
     for row in table.findAll('tr'):
@@ -57,8 +56,6 @@ for table in soup.findAll("table"):
       rowcount += 1
       cols = row.findAll(['td','th'])
       print(f"Found {len(cols)} columns.")
-      #TODO: Detect and discard empty tables (those that contain 1 row, 1 column, consisting of an empty string...I think?)
-      #TODO: Add a warning for non-rectangular tables.
       if cols:
         cols = [str(x.text).strip() for x in cols]
         if (len(cols) > 1) or len(cols[0]) > 0 :
@@ -68,7 +65,3 @@ for table in soup.findAll("table"):
   if args.ignoreempty and tableisempty:
     print(f"Removing {outfilename} because it is empty and `-e` flag was activated.")
     os.remove(outfilename)
-#TODO: In HTML documents that use tables for layout (yuck!) add option to detect table titles in preceding sibling tables of size 1x1
-#TODO: Add option to compress multi-line categorical headers (eg: headers with colspan>1) into concatenated-naming-style single row headers
-#  EG: "Population" header spanning above sub-headers "Number" and "Percentage" produces two single-column headers of "Population - Number"
-#  and "Population - Percentage". Delemiter can be configurable with a sensible default set.
